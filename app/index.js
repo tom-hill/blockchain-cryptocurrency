3f45dfe468ec395de9edd76b572f610fda81f08e
Tom Hill <tp.hill.uk@gmail.com>
2018-09-27 19:05:14 +0100

    Added simple /blocks and /mine endpoints to the API

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@
  */
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
@@ -11,8 +12,20 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const blockchain = new Blockchain();
 
+app.use(bodyParser.json());
+
 app.get('/blocks', (req, res) => {
     res.json(blockchain.chain);
+});
+
+app.post('/mine', (req, res) => {
+    const block = blockchain.addBlock(req.body.data);
+    console.log(`New block added:
+    =============================
+    ${block.toString()}
+    =============================`);
+
+    res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Local Server running on localhost:${HTTP_PORT}`));
