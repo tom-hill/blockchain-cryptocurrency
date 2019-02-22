73b4de62c44e68fbf98553b9f6510b151352da7b
Tom Hill <tom.hill@rushpartners.co.uk>
2019-02-22 15:03:58 +0000

    feat(block): Proof of work

--- blockchain/block.js
+++ blockchain/block.js
@@ -5,41 +5,51 @@
 
 const SHA3 = require('crypto-js/sha3');
 
+const DIFFICULTY = 4;
+
 class Block {
     static genesis() {
         const timestamp = 'Genesis Timestamp';
         const lastHash = SHA3('No previous hash').toString();
         const hash = Block.generateHash(timestamp, lastHash, []);
-        return new this(timestamp , lastHash, hash, []);
+        return new this(timestamp , lastHash, hash, [], 0);
     }
 
     static mineBlock(lastBlock, data) {
-        const timestamp = +new Date();
+        let hash, timestamp;
         const lastHash = lastBlock.hash;
-        const hash = Block.generateHash(timestamp, lastHash, data);
 
-        return new this(timestamp, lastHash, hash, data);
+        let nonce = 0;
+        do {
+            nonce ++;
+            timestamp = +new Date();
+            hash = Block.generateHash(timestamp, lastHash, data, nonce);
+        } while (hash.substr(0, DIFFICULTY) !== '0'.repeat(DIFFICULTY));
+
+        return new this(timestamp, lastHash, hash, data, nonce);
     }
 
-    static generateHash(timestamp, lasthash, data) {
-        return SHA3(`${timestamp}${lasthash}${data}`).toString();
+    static generateHash(timestamp, lasthash, data, nonce) {
+        return SHA3(`${timestamp}${lasthash}${data}${nonce}`).toString();
     }
 
     static getBlockHash(block) {
         const {
             timestamp,
             lastHash,
-            data
+            data,
+          nonce
         } = block;
 
-        return Block.generateHash(timestamp, lastHash, data);
+        return Block.generateHash(timestamp, lastHash, data, nonce);
     }
 
-    constructor(timestamp, lastHash, hash, data) {
+    constructor(timestamp, lastHash, hash, data, nonce) {
         this.timestamp = timestamp;
         this.lastHash = lastHash;
         this.hash = hash;
         this.data = data;
+        this.nonce = nonce;
     }
 
     toString() {
@@ -47,7 +57,8 @@ class Block {
             Timestamp: ${this.timestamp}
             Last Hash: ${this.lastHash.toString()}
             Hash     : ${this.hash.toString()}
-            Data     : ${this.data}`;
+            Data     : ${this.data}
+            Nonce    : ${this.nonce}`;
     }
 
     getGenesis() {
